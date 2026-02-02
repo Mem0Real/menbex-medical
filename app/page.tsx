adf467a552e9369b4e7d398159e0798f12b22446
Mem0Real <juniourmimo@gmail.com>
2026-02-02 10:49:09 +0300

    landing page generic

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-4xl">Hello World</h1>;
+  return (
+    <div className="bg-neutral-800 flex flex-col justify-start items-start w-full min-h-screen">
+      <div className="pt-8 ps-4">
+        <h1 className="text-4xl font-black font-geistMono">Menbex Medical</h1>
+      </div>
+    </div>
+  );
 }
